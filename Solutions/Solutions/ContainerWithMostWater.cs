8a050df02783cc7dceb5d9bf034325e4ff74af86
stephane kibonge <stephane.kibonge@etion.co.za>
2020-04-06 14:23:01 +0200

    Container With most water solved in n square

--- Solutions/Solutions/ContainerWithMostWater.cs
+++ Solutions/Solutions/ContainerWithMostWater.cs
@@ -10,7 +10,57 @@ namespace Solutions
         }
         public int MaxArea(int[] height)
         {
-            return 0;
+            int maxArea = 0;
+            int maxAreaLeft = 0;
+            int maxAreaRight = 0;
+
+            int localArea = 0;
+            int delta = -1;
+
+            int r = 0;
+            int l = 0;
+
+
+            // Walk through the array
+            for(int i = 0; i < height.Length; i++)
+            {
+
+
+
+                // Look for the furthest higher height on the right 
+                r = height.Length - 1;
+                l = i;
+                while (height[r] < height[l] && --r > l) { }
+
+
+                localArea = (height[r] > height[l] ? height[l] : height[r]) * (r - l);
+                if (r > l && localArea > maxArea) {
+                    maxAreaLeft = l;
+                    maxAreaRight = r;
+                    maxArea = localArea;
+                }
+
+
+
+
+                // Look for the furthest higher height on the right 
+                r = i;
+                l = 0;
+                while (height[l] < height[r] && r > ++l) { }
+
+
+                localArea = (height[r] > height[l] ? height[l] : height[r]) * (r - l);
+                if (r > l && localArea > maxArea)
+                {
+                    maxAreaLeft = l;
+                    maxAreaRight = r;
+                    maxArea = localArea;
+                }
+
+
+
+            }
+            return maxArea;
         }
     }
 }
--- Tests/Tests/ContainerWithMostWater.Tests.cs
+++ Tests/Tests/ContainerWithMostWater.Tests.cs
@@ -2,7 +2,7 @@ using NUnit.Framework;
 using System;
 using System.IO;
 using System.Collections.Generic;
-
+using Solutions;
 
 
 namespace Tests
@@ -10,33 +10,36 @@ namespace Tests
     public class ContainerWithMostWaterTests
     {
         private const string Expected = "Hello World!";
+        private ContainerWithMostWater solution;
 
         [SetUp]
         public void Setup()
         {
+            solution = new ContainerWithMostWater();
             //using (var sw = new StringWriter())
             //{
             //    Console.SetOut(sw);
             //    Solutions.ContainerWithMostWater.Main();
-            //
-            //    var result = sw.ToString().Trim();
+            
+            ///    var result = sw.ToString().Trim();
             //    Assert.AreEqual(Expected, result);
             //}
         }
 
         [Test]
         [TestCaseSource(nameof(TestMethod1_DataSource))]
-        public void TestMethod1(int[] arr)
+        public void TestMethod1(int[] arr, int exp)
         {
-            
-            Console.WriteLine(String.Join(", ", arr));
-            Assert.AreEqual(1, 1);
+            int res = solution.MaxArea(arr);
+            //Console.WriteLine($"{res} - {exp}");
+            //Console.WriteLine(String.Join(", ", arr));
+            Assert.AreEqual(res, exp);
             //Assert.Fail();
         }
         static IEnumerable<object[]> TestMethod1_DataSource()
         {
             return new[] {
-                new object[] { new int[] { 1, 8, 6, 2, 5, 4, 8, 3, 7 } },
+                new object[] { new int[] { 1, 8, 6, 2, 5, 4, 8, 3, 7 }, 49},
             };
         }
     }
