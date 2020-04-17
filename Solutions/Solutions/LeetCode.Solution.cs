e6962e5ba52001e4d59616cf6a6f95d8ee5fb12e
stephane kibonge <stephane.kibonge@etion.co.za>
2020-04-17 09:41:01 +0200

    Add Solution to string to integer (atoi)

--- Solutions/Solutions/LeetCode.Solution.cs
+++ Solutions/Solutions/LeetCode.Solution.cs
@@ -4,7 +4,7 @@ using System.Text;
 using System.Linq;
 
 // https://docs.microsoft.com/en-us/visualstudio/test/getting-started-with-unit-testing?view=vs-2019
-// https://leetcode.com/problems/zigzag-conversion/
+// https://leetcode.com/problems/string-to-integer-atoi/
 
 namespace LeetCode
 {
@@ -18,60 +18,93 @@ namespace LeetCode
     public class Solution
     {
 
-        public static string ExecuteWith(string s, int numRows)
+        public static int ExecuteWith(string str)
         {
-            int n = s.Length;
-            char[] s_zigzag = new char[n];
-
-
-            /// Solution is built around the goal of doing one pass
-            /// and therefore completely determining the coordinate of
-            /// the final poisition of the curretn char in the final string
-            /// as a function of the coordinate x and y in the zigzag
-
-            int mod = Math.Max(numRows + numRows - 2, 1); // Length of a zizag cycle
-            int cycles = (int)Math.Floor((double)n / mod); // number of zigzag cycles in current string 
-
-
-
-            int pos;
-            int index = 0;
-            int[] y_arr;
-
-
-            // y coordinate - rows
-            for (int y = 0; y < numRows; y++)
+            // Discard a null string
+            if (str.Length == 0)
             {
-                // in each cycle, we have 2 points per row
-                y_arr = new int[] { y, mod - y };
-                // Except for coordinate 0 and last row - only 1 point
-                if (y == 0 || y == numRows - 1)
-                {
-                    y_arr = new int[] { y };
-                }
-
-
-                // x coordinate - cols
-                for (int x = 0; x <= cycles; x++)
-                {
-
-                    foreach (int tmp_y in y_arr)
-                    {
-                        // Extracting curr char as function of coordinates
-                        pos = x * mod + tmp_y;
-                        if (pos < n)
-                        {
-                            // Storing current char in its final position
-                            // Setup index for next char
-                            s_zigzag[index++] = s[pos];
-                        }
-                    }
-                }
+                return 0;
             }
 
 
+            // Initialize variables
+            int i = 0;
+            int curr = 0;
+            int num = 0, tmp = 0, tmp2 = 0;
+            int maxPreMultiplicationBy10 = Int32.MaxValue / 10;
+            bool isNeg = false;
 
-            return new string(s_zigzag);
+
+
+            // Handle white spaces
+            if (i == str.Length) { return 0; }
+            while ((int)str[i] == 32) { i++; if (i == str.Length) { return 0; } }
+            if (i == str.Length) { return 0; }
+
+
+
+            // Handle plus, minus, or anything that's not a number
+            curr = (int)str[i];
+            if (curr == 43) { i++; }
+            else if (curr == 45) { i++; isNeg = true; }
+            else if (curr < 48) { return 0; }
+            else if (curr > 57) { return 0; }
+
+
+
+            // Handle any sequences of 0s
+            if (i == str.Length) { return 0; }
+            while ((int)str[i] == 48) { i++; if (i == str.Length) { return 0; } }
+            if (i == str.Length) { return 0; }
+
+
+
+            // if we don't have a digit next, return 0
+            curr = (int)str[i];
+            if (curr < 48) { return 0; }
+            else if (curr > 57) { return 0; }
+
+
+
+            // if we do have a digit, start computing (multiply num by 10, then add curr digit)
+            while (curr >= 48 && curr <= 57)
+            {
+                // multiply by 10
+                tmp = (num << 3) + (num << 1);
+
+                // add current digit
+                tmp2 = tmp + (curr - 48);
+
+                // check that there was no overflow
+                if (num > maxPreMultiplicationBy10 || tmp >> 31 != tmp2 >> 31) // Overflow on operatio over signed numbers
+                {
+                    // there is an overflow on x+y=z iif (msb of x != msb of z) or (msb of x != msb of z)
+                    // https://stackoverflow.com/questions/10078778/bitwise-overflow-checking-in-c
+                    // there is an overflow on x*y=z if x > max/10 or y > max/10
+                    // https://stackoverflow.com/questions/1815367/catch-and-compute-overflow-during-multiplication-of-two-large-integers
+
+                    // if there was an overflow, go to max integer
+                    num = Int32.MaxValue;
+                    return isNeg ? ++num : num;
+                }
+                else
+                {
+                    num = tmp2;
+                }
+
+
+
+
+                // Prepare next cycle
+                i++;
+                // if we have reached the end of the string, that's it
+                if (i == str.Length) { break; }
+                // Extract next digit, and prep curr for the next iteration
+                curr = (int)str[i];
+            }
+
+
+            return isNeg ? 0 - num : num;
         }
     }
 
--- Tests/Tests/LeetCode.Tests.cs
+++ Tests/Tests/LeetCode.Tests.cs
@@ -22,10 +22,10 @@ namespace LeetCodeTests
 
         [Test]
         [TestCaseSource(nameof(TestMethod1_DataSource))]
-        public void TestMethod1(string input, int numRows, string exp)
+        public void TestMethod1(string input, int exp)
         {
 
-            var res = Solution.ExecuteWith(input, numRows);
+            var res = Solution.ExecuteWith(input);
 
 
             Assert.AreEqual(res, exp);
@@ -34,8 +34,11 @@ namespace LeetCodeTests
         static IEnumerable<object[]> TestMethod1_DataSource()
         {
             return new[] {
-                new object[] { "PAYPALISHIRING", 3, "PAHNAPLSIIGYIR" },
-                new object[] { "PAYPALISHIRING", 4, "PINALSIGYAHRPI" },
+                new object[] { "42", 42 },
+                new object[] { "-9128347233", -2147483648 },
+                new object[] { "words and 987", 0 },
+                new object[] { "    -42", -42 },
+                new object[] { "4193 with words", 4193 },
             };
         }
 
