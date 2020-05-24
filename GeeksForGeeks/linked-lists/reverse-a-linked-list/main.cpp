26b89089b7f4d970437808ce827484dca10a68ec
stephane kibonge <stephane.kibonge@etion.co.za>
2020-05-24 12:26:04 +0200

    Add solution to 'reverse-a-linked-list'

---
+++ GeeksForGeeks/linked-lists/reverse-a-linked-list/main.cpp
@@ -0,0 +1,76 @@
+
+// C program for flattening a linked list 
+#include <stdio.h> 
+#include <stdlib.h> 
+
+#include <bits/stdc++.h>
+using namespace std;
+
+// A Linked List Node 
+typedef struct Node 
+{ 
+    int data; 
+    struct Node *next; 
+    Node(int d) : data(d){};
+    Node(int d, Node *next) : data(d), next(next){};
+} Node; 
+
+void printList(Node *node) 
+{ 
+    printf("\n");
+    while (node != NULL) 
+    { 
+        printf("%d ", node->data); 
+        node = node->next; 
+    } 
+}   
+Node *buildLinkedList(vector<int> data){
+    Node *root = new Node(data[0]), *curr; 
+    curr = root;
+    
+    for(int i = 1; i < data.size(); i++){
+        curr->next = new Node(data[i]);
+        curr = curr->next;
+    }
+    curr->next = nullptr;
+    return root;
+}
+
+
+
+Node* reverse(Node* head) {
+
+    if(head == nullptr) return head;
+
+    Node *backward = head;
+    Node *forward = head->next;
+    Node *tmp;
+
+    backward->next = nullptr;
+
+    while(forward != nullptr){
+        tmp = forward->next;
+        forward->next = backward;
+        backward = forward;
+        forward = tmp;
+    }
+
+    return backward;
+}
+
+
+
+// Driver program to test above functions 
+int main() 
+{ 
+
+    // Let us flatten the list 
+    Node *root = reverse(
+        buildLinkedList({1,2,3,4,5,6,7,8,9})
+    ); 
+
+    // Let us print the flatened linked list 
+    printList(root); 
+
+    return 0; 
+} 
