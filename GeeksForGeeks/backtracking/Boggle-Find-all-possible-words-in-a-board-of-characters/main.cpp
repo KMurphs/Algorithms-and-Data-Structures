bca3f2fd5bdaa6c2662ef0d727620ef970a60bbc
stephane kibonge <stephane.kibonge@etion.co.za>
2020-05-26 22:12:42 +0200

    Update solution to 'Boggle-Find-all-possible-words-in-a-board-of-characters'. size of array pointer

--- GeeksForGeeks/backtracking/Boggle-Find-all-possible-words-in-a-board-of-characters/main.cpp
+++ GeeksForGeeks/backtracking/Boggle-Find-all-possible-words-in-a-board-of-characters/main.cpp
@@ -8,17 +8,33 @@ using namespace std;
 // A given function to check if a given string is present in 
 // dictionary. The implementation is naive for simplicity. As 
 // per the question dictionary is given to us. 
-bool isInDictionary(string dictionary[], string& str) 
+bool isInDictionary(vector<string> dictionary, string& str) 
 {   
-    int n = sizeof(dictionary); 
+    // int n = *(&dictionary + 1) - dictionary; 
+    int n = dictionary.size(); 
     // Linearly search all words 
     for (int i = 0; i < n; i++) 
         if (str.compare(dictionary[i]) == 0) 
             return true; 
     return false; 
 } 
+    // Probably only works with ints
+    // https://www.quora.com/What-is-the-equivalent-of-array-length-used-in-Java-in-C++
+    // int size = *(&arr + 1) - arr;
+    // &arr ==> Pointer to an array of 6 elements.
+    //          [See this for difference between &arr
+    //           and arr]   
+    // (&arr + 1) ==> Address of 6 integers ahead as
+    //                pointer type is pointer to array
+    //                of 6 integers.
+    // *(&arr + 1) ==> Same address as (&arr + 1), but 
+    //                 type of pointer is "int *".
+    // *(&arr + 1) - arr ==> Since *(&arr + 1) points 
+    //                    to the address 6 integers
+    //                    ahead of arr, the difference
+    //                    between two is 6.          
 
-void dfsUtil(string dictionary[], char boggle[M][N], vector<string>& sols, bool visited[3][3], int x, int y, string currChars){
+void dfsUtil(vector<string> dictionary, char boggle[M][N], vector<string>& sols, bool visited[3][3], int x, int y, string currChars){
   
   visited[x][y] = 1;
   currChars = currChars + boggle[x][y];
@@ -46,7 +62,7 @@ void dfsUtil(string dictionary[], char boggle[M][N], vector<string>& sols, bool 
   visited[x][y] = 0;
 }
 
-vector<string> dfs(string dictionary[], char boggle[M][N]){
+vector<string> dfs(vector<string> dictionary, char boggle[M][N]){
   vector<string> words;
   string str = ""; 
 
@@ -63,7 +79,7 @@ vector<string> dfs(string dictionary[], char boggle[M][N]){
 
 int main(){
 
-    string dictionary[] = { "GEEKS", "FOR", "QUIZ", "GO" }; 
+    vector<string> dictionary = { "GEEKS", "FOR", "QUIZ", "GO" }; 
     char boggle[3][3]   = {{'G', 'I', 'Z'}, {'U', 'E', 'K'}, {'Q', 'S', 'E'}};
 
     vector<string> sols {"GEEKS","QUIZ"};
