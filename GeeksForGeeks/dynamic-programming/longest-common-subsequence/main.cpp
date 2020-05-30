f5292be5ac39e0ae2e4790f85d9ae02f7d8c1fee
stephane kibonge <stephane.kibonge@etion.co.za>
2020-05-30 14:22:19 +0200

    Add solution to 'longest-common-subsequence'

--- GeeksForGeeks/dynamic-programming/longest-common-subsequence/main.cpp
+++ GeeksForGeeks/dynamic-programming/longest-common-subsequence/main.cpp
@@ -1,102 +1,55 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+// http://www.parashift.com/c++-faq/
 
-#define M 3 
-#define N 3 
+int lcs(char x[], char y[], int xLen, int yLen){
 
-// A given function to check if a given string is present in 
-// dictionary. The implementation is naive for simplicity. As 
-// per the question dictionary is given to us. 
-bool isInDictionary(vector<string> dictionary, string& str) 
-{   
-    // int n = *(&dictionary + 1) - dictionary; 
-    int n = dictionary.size(); 
-    // Linearly search all words 
-    for (int i = 0; i < n; i++) 
-        if (str.compare(dictionary[i]) == 0) 
-            return true; 
-    return false; 
-} 
-    // Probably only works with ints
-    // https://www.quora.com/What-is-the-equivalent-of-array-length-used-in-Java-in-C++
-    // int size = *(&arr + 1) - arr;
-    // &arr ==> Pointer to an array of 6 elements.
-    //          [See this for difference between &arr
-    //           and arr]   
-    // (&arr + 1) ==> Address of 6 integers ahead as
-    //                pointer type is pointer to array
-    //                of 6 integers.
-    // *(&arr + 1) ==> Same address as (&arr + 1), but 
-    //                 type of pointer is "int *".
-    // *(&arr + 1) - arr ==> Since *(&arr + 1) points 
-    //                    to the address 6 integers
-    //                    ahead of arr, the difference
-    //                    between two is 6.          
+  int dp[xLen + 1][yLen + 1];
 
-void dfsUtil(vector<string> dictionary, char boggle[M][N], vector<string>& sols, bool visited[3][3], int x, int y, string currChars){
-  
-  visited[x][y] = 1;
-  currChars = currChars + boggle[x][y];
-
-
-  if(isInDictionary(dictionary, currChars)){
-    sols.push_back(currChars);
-    // return;
-  }
-
-
-  for(int i = -1; i <= 1; i++)
-    for(int j = -1; j <= 1; j++)
-      if( 
-        x + i < M && x + i >= 0 &&
-        y + j < N && y + j >= 0 &&
-        !visited[x + i][y + j]
-      ){
-        // currChars.push_back(boggle[x + i][y + j]);
-        dfsUtil(dictionary, boggle, sols, visited, x + i, y + j, currChars);
-        // currChars.pop_back();
-      }
-
-  currChars.erase(currChars.length() - 1);
-  visited[x][y] = 0;
-}
-
-vector<string> dfs(vector<string> dictionary, char boggle[M][N]){
-  vector<string> words;
-  string str = ""; 
-
-  bool visited[M][N] = { { false } };
-
-
-  for(int i = 0; i < 3; i++)
-    for(int j = 0; j < 3; j++)
-      dfsUtil(dictionary, boggle, words, visited, i, j, str);
-
-  return words;
-}
-
-
-int main(){
-
-    vector<string> dictionary = { "GEEKS", "FOR", "QUIZ", "GO" }; 
-    char boggle[3][3]   = {{'G', 'I', 'Z'}, {'U', 'E', 'K'}, {'Q', 'S', 'E'}};
-
-    vector<string> sols {"GEEKS","QUIZ"};
-
-    vector<string> foundWords = dfs(dictionary, boggle);
-
-    assert(sols.size() == foundWords.size());
-    for(int i = 0; i < sols.size(); i++){
-      bool found = false;
-      for(int j = 0; j < foundWords.size(); j++){
-        found = found || sols[i] == foundWords[j];
-        if(found) break;
-      }
-      assert(found);
+  for(int i = 0 ; i <= xLen; i++)
+    for(int j = 0; j <= yLen; j++){
+      if(i == 0 || j == 0) 
+        dp[i][j] = 0;
+      else
+        if(x[i - 1] == y[j - 1])
+          dp[i][j] = 1 + dp[i - 1][j - 1];
+        else
+          dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
     }
 
+  return dp[xLen][yLen];
+}
 
-    cout << "Program is exiting..." << endl; 
+// int lcs(char y[], char x[], int yLen, int xLen){
+
+//   int dp[xLen + 1][yLen + 1];
+
+//   for(int i = 0 ; i <= xLen; i++)
+//     for(int j = 0; j <= yLen; j++){
+//       if(i == 0 || j == 0) 
+//         dp[i][j] = 0;
+//       else{
+//         printf("\n\nProcessing: (%c(%d), %c(%d)) - %d", x[i - 1], i, y[j - 1], j, dp[i][j]);
+//         dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]) + (x[i - 1] == y[j - 1] ? 1 : 0);
+//         printf("\nSides: %d, %d - Max: %d - Current: %d", dp[i - 1][j], dp[i][j - 1], max(dp[i - 1][j], dp[i][j - 1]), dp[i][j]);
+//       }
+        
+//     }
+
+//   return dp[xLen][yLen];
+// }
+int main(){
+
+    char X[] = "AGGTAB";  
+    char Y[] = "GXTXAYB";  
+      
+    int m = strlen(X);  
+    int n = strlen(Y);  
+    int res = lcs( X, Y, m, n );
+    cout << "Length of LCS is "<< res;  
+    assert(res == 4);
+
+    cout << "\n\nProgram is exiting..." << endl; 
     return 0; 
 }
