09d0e238964541ad75f23de78f563ebd3a1d605b
stephane kibonge <stephane.kibonge@etion.co.za>
2020-05-30 14:26:03 +0200

    Fix solution to 'longest-common-subsequence' to use stl vector instead of 2D array

--- GeeksForGeeks/dynamic-programming/longest-common-subsequence/main.cpp
+++ GeeksForGeeks/dynamic-programming/longest-common-subsequence/main.cpp
@@ -5,7 +5,14 @@ using namespace std;
 
 int lcs(char x[], char y[], int xLen, int yLen){
 
-  int dp[xLen + 1][yLen + 1];
+  vector<vector<int>> dp;
+  for(int i = 0 ; i <= xLen; i++){
+    vector<int> tmp;
+    for(int j = 0; j <= yLen; j++)
+      tmp.push_back(0);
+    dp.push_back(tmp);
+  }
+
 
   for(int i = 0 ; i <= xLen; i++)
     for(int j = 0; j <= yLen; j++){
@@ -21,24 +28,7 @@ int lcs(char x[], char y[], int xLen, int yLen){
   return dp[xLen][yLen];
 }
 
-// int lcs(char y[], char x[], int yLen, int xLen){
 
-//   int dp[xLen + 1][yLen + 1];
-
-//   for(int i = 0 ; i <= xLen; i++)
-//     for(int j = 0; j <= yLen; j++){
-//       if(i == 0 || j == 0) 
-//         dp[i][j] = 0;
-//       else{
-//         printf("\n\nProcessing: (%c(%d), %c(%d)) - %d", x[i - 1], i, y[j - 1], j, dp[i][j]);
-//         dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]) + (x[i - 1] == y[j - 1] ? 1 : 0);
-//         printf("\nSides: %d, %d - Max: %d - Current: %d", dp[i - 1][j], dp[i][j - 1], max(dp[i - 1][j], dp[i][j - 1]), dp[i][j]);
-//       }
-        
-//     }
-
-//   return dp[xLen][yLen];
-// }
 int main(){
 
     char X[] = "AGGTAB";  
@@ -46,6 +36,7 @@ int main(){
       
     int m = strlen(X);  
     int n = strlen(Y);  
+
     int res = lcs( X, Y, m, n );
     cout << "Length of LCS is "<< res;  
     assert(res == 4);
