d66e08eb724226bd279d8aacee2633d3c3091c18
stephane kibonge <stephane.kibonge@etion.co.za>
2020-05-30 14:29:18 +0200

    Fix solution to 'longest-common-subsequence' to use stl vector instead of 2D array

--- GeeksForGeeks/dynamic-programming/longest-common-subsequence/main.cpp
+++ GeeksForGeeks/dynamic-programming/longest-common-subsequence/main.cpp
@@ -14,7 +14,7 @@ int lcs(char x[], char y[], int xLen, int yLen){
   }
 
 
-  for(int i = 0 ; i <= xLen; i++)
+  for(int i = 0 ; i <= xLen; i++){
     for(int j = 0; j <= yLen; j++){
       if(i == 0 || j == 0) 
         dp[i][j] = 0;
@@ -24,7 +24,8 @@ int lcs(char x[], char y[], int xLen, int yLen){
         else
           dp[i][j] = max(dp[i - 1][j], dp[i][j - 1]);
     }
-
+  }
+  
   return dp[xLen][yLen];
 }
 
