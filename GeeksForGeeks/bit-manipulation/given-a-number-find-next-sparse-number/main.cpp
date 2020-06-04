01642c902a40169287cb21898c076798cc537f01
stephane kibonge <stephane.kibonge@etion.co.za>
2020-06-04 11:13:19 +0200

    Update solution to 'given-a-number-find-next-sparse-number' by using fact that '(INT_MIN >> i) + (1 << (nBits - i - 1)) == 0'

--- GeeksForGeeks/bit-manipulation/given-a-number-find-next-sparse-number/main.cpp
+++ GeeksForGeeks/bit-manipulation/given-a-number-find-next-sparse-number/main.cpp
@@ -10,17 +10,23 @@ int getNextSparseNumber(int num){
   
   int nBits = 8 * sizeof(num);
   unsigned int pattern_00 = 3, pattern_01 = 11;
-  int mask, setter;
+  int mask;//, setter;
 
-
+  assert(INT_MIN == (1 << (nBits - 1)));
+  assert((INT_MIN >> 7) + (1 << (nBits - 7 - 1)) == 0);
+  assert((INT_MIN >> 9) + (1 << (nBits - 10)) == 0);
+  // Since relations above is true, we can avoid keeping track of setter
+  // if i is not 1: (INT_MIN >> i) + (1 << (nBits - i)) == 0
+  // so setter = 0 - mask
   mask = INT_MIN >> 1; //setter = 1 << (nBits - 2) or setter = 0 - mask;
-  setter = 1 << (nBits - 2);
-
+  // setter = 1 << (nBits - 2);
+  
+  
 
   pattern_00 = pattern_00 << (nBits - 4);
   pattern_01 = pattern_01 << (nBits - 4);
   mask = mask >> 2; //setter = 1 << (nBits - 2) or setter = 0 - mask;
-  setter = setter >> 2;
+  //setter = setter >> 2;
 
   if((num & (pattern_00 << 2)) == (pattern_00 << 2)) return 0;
   if((num & (pattern_00 << 1)) == (pattern_00 << 1)) return INT_MIN;
@@ -31,16 +37,16 @@ int getNextSparseNumber(int num){
     pattern_00 = pattern_00 >> 1;
     pattern_01 = pattern_01 >> 1;
     mask = mask >> 1;
-    setter = setter >> 1;
+    //setter = setter >> 1;
   }
 
   if((num & pattern_00) == pattern_00){
 
-    mask = mask << 2, setter = setter << 2;
-    if((num & pattern_01) == pattern_01) mask = mask << 2, setter = setter << 2;
+    mask = mask << 2;//, setter = setter << 2;
+    if((num & pattern_01) == pattern_01) mask = mask << 2;//, setter = setter << 2;
 
     num = num & mask;
-    num = num | setter;
+    num = num | (0 - mask); // if mask is '1110 0000' then (0 - mask) is '0010 0000'
   }
 
   return num;
