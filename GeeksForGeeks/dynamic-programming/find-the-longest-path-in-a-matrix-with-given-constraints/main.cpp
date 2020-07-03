0828fa2fbc1b64cf2fe160fc9bafadba1b44b20b
stephane kibonge <stephane.kibonge@etion.co.za>
2020-07-03 16:21:50 +0200

    Fix bug in solution for 'find-the-longest-path-in-a-matrix-with-given-constraints'

--- GeeksForGeeks/dynamic-programming/find-the-longest-path-in-a-matrix-with-given-constraints/main.cpp
+++ GeeksForGeeks/dynamic-programming/find-the-longest-path-in-a-matrix-with-given-constraints/main.cpp
@@ -46,6 +46,92 @@ int findLongestPath(int mat[n][n]){
   return maxPathLength;
 }
 
+
+
+
+
+
+
+
+
+
+
+bool isSafe(int mat[n][n], int row, int col){
+  if(row < 0 || row >= n) return false;
+  if(col < 0 || col >= n) return false;
+  return true;
+}
+
+#define INCREMENTS 4
+struct TIncrement{
+  int drow, dcol;
+};
+TIncrement increments[INCREMENTS] = {
+  {0, -1},
+  {0, 1},
+  {1, 0},
+  {-1, 0},
+};
+
+
+
+struct Elmt{
+  int val, row, col;
+};
+int findLongestPath2(int mat[n][n]){
+
+
+  // Sort matrix elemnts, Store along with value position of the elemnt in matrix
+  Elmt *elmts = new Elmt[n*n];
+
+  for(int i = 0, k = 0 ; i < n ; i++) 
+    for(int j = 0 ; j < n ; j++)
+      elmts[k++] = { mat[i][j], i, j };
+
+  sort(elmts, elmts + (n*n), [](const Elmt& elmt1, const Elmt& elmt2) {
+    return elmt1.val < elmt2.val;
+  });
+
+
+  // Prepare dp structure, and set to 0's
+  int **dp = new int*[n];
+  for(int i = 0 ; i < n ; i++){
+    dp[i] = new int[n];
+    memset(dp[i], 0 , sizeof(int) * n);
+  }
+
+
+
+  // dp algo
+  int maxPath = INT_MIN;
+  for(int i = 0 ; i < n*n ; i++){
+    for(int j = 0 ; j < INCREMENTS ; j++){
+
+      // Compute next logical point in all four direction
+      int nextRow = elmts[i].row + increments[j].drow;
+      int nextCol = elmts[i].col + increments[j].dcol;
+
+      // If point is valid and greater than current point
+      if(isSafe(mat, nextRow, nextCol) && mat[nextRow][nextCol] > elmts[i].val){
+        dp[nextRow][nextCol] = max(dp[elmts[i].row][elmts[i].col] + 1, dp[nextRow][nextCol]);
+
+        // Keep track of maxPath Length
+        if(dp[nextRow][nextCol] > maxPath) 
+          maxPath = dp[nextRow][nextCol];
+      }
+    }
+  } 
+
+
+  return maxPath;
+}
+
+
+
+
+
+
+
 // https://en.cppreference.com/w/c/language/main_function
 // arguments count, 
 // arguments vector, 
@@ -58,7 +144,8 @@ int main(int argc, char **argv, char **envp){
   int maxPathLength = findLongestPath(mat);
   cout << "Longest Path is of Length: " << maxPathLength << endl;
 
-  assert(maxPathLength == 4);
+  assert(maxPathLength == 6);
+  assert(findLongestPath2(mat) == 6);
 
   cout << "\nProgram Exited successfully";
   return 0;
