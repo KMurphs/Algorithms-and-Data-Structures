78675a9dc8ab0dc9d06a9a42bf3332b56cd0dd60
stephane kibonge <stephane.kibonge@etion.co.za>
2020-07-08 17:14:46 +0200

    Add dynamic programming solution to 'matrix-chain-multiplication-dp-8'

--- GeeksForGeeks/dynamic-programming/matrix-chain-multiplication-dp-8/main.cpp
+++ GeeksForGeeks/dynamic-programming/matrix-chain-multiplication-dp-8/main.cpp
@@ -7,7 +7,10 @@ struct TSolution{
   int rows, cols, ops;
   TSolution(): rows(0), cols(0), ops(0){}
 };
-
+// combinatorial exercise
+// recursive brute force approach (optimal substructure)
+// overlapping subproblems (the same 3 consecutive elemnts are reused 
+// all over the palce)
 TSolution solve(int *mats, int n, int start, int end){
 
   TSolution res = *new TSolution();
@@ -43,6 +46,51 @@ int reduceTotalOperations(int *mats, int n){
 }
 
 
+
+
+
+
+
+
+
+
+
+
+
+int reduceTotalOperationsDP(int *mats, int n){
+
+
+  int **dp = new int*[n];
+  for (int i = 0; i < n; i++){
+    dp[i] = new int[n];
+    memset(dp[i], 0, sizeof(int) * n);
+  }
+  
+
+  for (int i = 0; i < n; i++){
+    dp[i][i] = 0;
+  }
+  for (int i = 0; i < n - 1; i++){
+    dp[i][i + 1] = 0;
+  }
+
+
+  for (int l = 2; l < n; l++){
+    for (int i = 0; i < n && i + l < n; i++){
+      dp[i][i + l] = min(
+        dp[i][i + l - 1] + mats[i] * mats[i + l - 1] * mats[i + l],
+        dp[i + 1][i + l] + mats[i + 1] * mats[i] * mats[i + l]
+      );
+    }
+  }
+
+
+  return dp[0][n - 1];
+}
+
+
+
+
 // https://www.geeksforgeeks.org/matrix-chain-multiplication-dp-8/
 int main(int argc, char **argv, char **envp){
 
@@ -53,16 +101,19 @@ int main(int argc, char **argv, char **envp){
   n = 5, mats = new int[n]{ 40, 20, 30, 10, 30 }, exp = 26000;
   cout << reduceTotalOperations(mats, n) << endl;
   assert(reduceTotalOperations(mats, n) == exp);
+  assert(reduceTotalOperationsDP(mats, n) == exp);
 
 
   n = 5, mats = new int[n]{ 10, 20, 30, 40, 30 }, exp = 30000;
   cout << reduceTotalOperations(mats, n) << endl;
   assert(reduceTotalOperations(mats, n) == exp);
+  assert(reduceTotalOperationsDP(mats, n) == exp);
 
 
   n = 3, mats = new int[n]{ 10, 20, 30 }, exp = 6000;
   cout << reduceTotalOperations(mats, n) << endl;
   assert(reduceTotalOperations(mats, n) == exp);
+  assert(reduceTotalOperationsDP(mats, n) == exp);
 
 
 
