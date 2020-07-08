7a9c7997187f5ac9ae046e67a26674ebcdb6523a
stephane kibonge <stephane.kibonge@etion.co.za>
2020-07-08 15:09:26 +0200

    Refine solution to 'shortest-common-supersequence'

--- GeeksForGeeks/dynamic-programming/shortest-common-supersequence/main.cpp
+++ GeeksForGeeks/dynamic-programming/shortest-common-supersequence/main.cpp
@@ -5,31 +5,15 @@ using namespace std;
 
 string solve(string s1, string s2, int i1, int i2){
 
-  cout << "        ***" << endl;
-  cout << s1 << "\t" << s2 << endl;
+  if(i2 + 1 == s2.size()) return s1;
+  if(i1 + 1 == s1.size()) return s1 + s2.substr(i2 + 1);
 
-  if(i1 == s1.size() && i2 == s2.size()) return s1;
-  if(i1 == s1.size()) return s1 + s2.substr(i2);
-  if(i2 == s2.size()) return s1;
-
-  string res1 = "", res2 = "", res3 = "";
-
-  res1 = solve((i1 > 0 ? s1.substr(0, i1) : "") + s2[i2] + s1.substr(i1), s2, i1 + 1, i2 + 1); // insert
-  // res1 = solve(s1.substr(0, i1 + 1) + s2[i2] + s1.substr(i1 + 1), s2, i1 + 1, i2 + 1); // insert
-  res2 = solve(s1, s2, i1 + 1, i2); // leave alone
-
-
-
-  cout << res1 << endl;
-  cout << res2 << endl;
-  
-
-  if(s1[i1] == s2[i2]){
-    res3 = solve(s1, s2, i1 + 1, i2 + 1);
-    cout << res3 << endl;
-    if(res3.size() < res1.size() && res3.size() < res2.size())
-      return res3;
+  if(s1[i1 + 1] == s2[i2 + 1]){
+    return solve(s1, s2, i1 + 1, i2 + 1);
   }
+
+  string res1 = solve((i1 >= 0 ? s1.substr(0, i1 + 1) : "") + s2[i2 + 1] + s1.substr(i1 + 1), s2, i1 + 1, i2 + 1); // insert
+  string res2 = solve(s1, s2, i1 + 1, i2); // leave alone
   
   return res1.size() < res2.size() ? res1 : res2;
 }
@@ -38,7 +22,7 @@ string solve(string s1, string s2, int i1, int i2){
 
 
 string buildShortestSuperSeq(string s1, string s2){
-  return solve(s1, s2, 0, 0);
+  return solve(s1, s2, -1, -1);
 }
 
 
