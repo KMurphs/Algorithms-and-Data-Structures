a07731c12c5423298829e9f32c283874660e0ad8
stephane kibonge <stephane.kibonge@etion.co.za>
2020-07-09 09:29:51 +0200

    Add solution to 'cutting-a-rod-dp-13 to maximise price'

--- GeeksForGeeks/dynamic-programming/partition-problem-dp-18/main.cpp
+++ GeeksForGeeks/dynamic-programming/partition-problem-dp-18/main.cpp
@@ -2,25 +2,31 @@
 using namespace std;
 
 
-// Optimization Problem
-// Recursive Combinatorial Solution (optimal substructure property)
+// Combinatorial Problem
+// Recursive Combinatorial Solution (optimal substructure property - state is localized)
 // Overlapping problems since any branch must generate and consider 
-// the same solution for the last 3 elemts 
-bool partitionUtil(int *arr, int n, int start, int sum1, int sum2){
+// the same solution for the all rods of length 3 for example
+int maximizeCutsUtil(int *lengths, int *prices, int n, int currLength, int currPrice){
 
-  if(start >= n){
-    return sum1 == sum2;
+  if(currLength == 0) return currPrice;
+  if(currLength <  0) return INT_MIN;
+
+  int tmpPrice, maxPrice = INT_MIN;
+  for (int i = 0; i < n; i++){
+
+    tmpPrice = maximizeCutsUtil(lengths, prices, n, currLength - lengths[i], currPrice + prices[i]);
+
+    if(tmpPrice > maxPrice){
+      maxPrice = tmpPrice;
+    }
   }
 
-  return partitionUtil(arr, n, start + 1, sum1, sum2 + arr[start])
-      || partitionUtil(arr, n, start + 1, sum1 + arr[start], sum2);
+  return maxPrice;
+}
+int maximizeCuts(int *lengths, int *prices, int n, int rodLength){
+  return maximizeCutsUtil(lengths, prices, n, rodLength, 0);
 }
 
-bool partition(int *arr, int n){
-  if(accumulate(arr, arr + n, 0) % 2 != 0)
-    return false;
-  return partitionUtil(arr, n, 0, 0, 0);
-}
 
 
 
@@ -32,26 +38,28 @@ bool partition(int *arr, int n){
 
 
 
+string printSolution(int *dp, int currLength){
 
-void printSolution(int **dp, int *arr, int row, int col){
-
-  if(row == 0){
-    cout << endl;
-    return;
+  if(currLength == 0){
+    return "";
   }
 
-  if(!dp[row - 1][col] && col >= arr[row - 1]){
-    cout << "Set 1: " << arr[row - 1] << endl;
-  }else{
-    cout << "Set 2: " << arr[row - 1] << endl;
+
+  int l = 1, r = currLength - 1;
+  bool found = false;
+  while(l <= r){
+    if(dp[currLength] == dp[l] + dp[r]){
+      found = true;
+      break;
+    }
+    l++, r--;
   }
 
-  if(dp[row - 1][col]){
-    printSolution(dp, arr, row - 1, col);
-  }else if(col >= arr[row - 1]){
-    printSolution(dp, arr, row - 1, col - arr[row - 1]);
+  if(!found){
+    return to_string(currLength);
   }
 
+  return printSolution(dp, l) + ", " + printSolution(dp, r);
 }
 
 // Altough the overlapping would suggest solving subproblems of 
@@ -61,56 +69,32 @@ void printSolution(int **dp, int *arr, int row, int col){
 // array elmts, vs set of all possible sums of subset 1 
 // The final solution will be borne out of the sum that is closest to
 // the middle of the maximum sum
-int partitionDP(int *arr, int n){
+int maximizeCutsDP(int *lengths, int *prices, int n, int rodLength){
 
+  int *dp = new int[rodLength + 1];
+  memset(dp, 0, sizeof(int) * (rodLength + 1)); 
 
-  const int sum = accumulate(arr, arr + n, 0);
-  if(sum % 2 != 0)
-    return false;
-
-  const int hSum = sum / 2;
-
-  int **dp = new int*[n + 1];
-  for(int i = 0 ; i < n + 1; i++){
-    dp[i] = new int[hSum + 1];
-    memset(dp[i], 0, sizeof(int) * (hSum + 1)); 
+  for(int i = 0 ; i < n ; i++){
+    dp[lengths[i]] = prices[i];
   }
 
+  for(int i = 1 ; i < rodLength + 1 ; i++){
 
-  // each cell will answer the question 
-  // can we get to this sum using previous and possibly current arr elemt?
-  for(int i = 1 ; i < n + 1 ; i++){
-    dp[i][0] = 1;
-    for (int j = 1; j < hSum + 1; j++){
-      
-      // first of all can we get to the current sum j
-      // by using previous set? If that's the case
-      // we don't need to include curr arr elemt
-      dp[i][j] = dp[i - 1][j];
-
-      if((dp[i][j] == 0) && (arr[i - 1] >= j)){
-        // We can't get to surr sum j using previous set.
-        // Can we alternatively get to currSum - currArrElemt?
-        // using previous set. If this is possible, then
-        // we can using previous set to get to (currSum - currArrElemt)
-        // and include currArrELemt, and increase this sum from
-        // (currSum - currArrElemt) to (currSum - currArrElemt + currArrElemt)
-        dp[i][j] = dp[i - 1][j - arr[i - 1]];
+    int l = 0, r = i;
+    while(l <= r){
+      if(dp[i] < dp[l] + dp[r]){
+        dp[i] = dp[l] + dp[r];
       }
+      l++, r--;
     }
+
   }
 
 
+  string res = printSolution(dp, rodLength);
+  cout << res << endl;
 
-
-
-  if(dp[n][hSum] == 1){
-    printSolution(dp, arr, n, hSum);
-  }
-
-
-
-  return dp[n][hSum];
+  return dp[rodLength];
 }
 
 
@@ -125,23 +109,18 @@ int partitionDP(int *arr, int n){
 
 
 
-// https://www.geeksforgeeks.org/partition-problem-dp-18/
+// https://www.geeksforgeeks.org/cutting-a-rod-dp-13/
 int main(){
 
-    int n, *arr;  
-    bool exp;
+    int n, *lengths, *prices, rodLength, exp;
 
-    n = 4, arr = new int[n]{ 1, 6, 11, 5 }, exp = 0;
-    assert(partition(arr, n) == exp);
-    assert(partitionDP(arr, n) == exp);
+    rodLength = 8, n = 8, lengths = new int[n]{ 1, 2, 3, 4, 5, 6, 7, 8 }, prices = new int[n]{ 1, 5, 8, 9, 10, 17, 17, 20 }, exp = 22;
+    assert(maximizeCuts(lengths, prices, n, rodLength) == exp);
+    assert(maximizeCutsDP(lengths, prices, n, rodLength) == exp);
 
-    n = 4, arr = new int[n]{ 1, 5, 11, 5 }, exp = 1;
-    assert(partition(arr, n) == exp);
-    assert(partitionDP(arr, n) == exp);
-    
-    n = 4, arr = new int[n]{ 1, 5, 3 }, exp = 0;
-    assert(partition(arr, n) == exp);
-    assert(partitionDP(arr, n) == exp);
+    rodLength = 8, n = 8, lengths = new int[n]{ 1, 2, 3, 4, 5, 6, 7, 8 }, prices = new int[n]{ 3, 5, 8, 9, 10, 17, 17, 20 }, exp = 24;
+    assert(maximizeCuts(lengths, prices, n, rodLength) == exp);
+    assert(maximizeCutsDP(lengths, prices, n, rodLength) == exp);
 
 
     cout << "\n\nProgram is exiting..." << endl; 
