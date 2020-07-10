4d145886eb3ad46a5b7ef9dc5f4a37dfd3d8a0da
stephane kibonge <stephane.kibonge@etion.co.za>
2020-07-10 13:52:58 +0200

    Add dynamic programming solution to 'maximum-product-cutting-dp-36'

--- GeeksForGeeks/dynamic-programming/maximum-product-cutting-dp-36/main.cpp
+++ GeeksForGeeks/dynamic-programming/maximum-product-cutting-dp-36/main.cpp
@@ -2,26 +2,77 @@
 #include <unordered_set>
 using namespace std;
 
+// 1. combinatorial solution
+// 2. recursive brute force
+// 3. contained recursive state
+// 4. overlapping subproblems
+// 5. ordering on dimensions (start and pos)
+// Note: exponential brute force => O(2^n)
 
-
-int wordBreakUtil(int n, int start, int pos){
+int maxProdCuttingUtil(int n, int start, int pos){
 
   if(pos - start >= n) return 0;
   if(start == n) return 1;
   if(pos > n) return 0;
 
   return max(
-    wordBreakUtil(n, start, pos + 1),
-    (pos - start) * wordBreakUtil(n, pos, pos + 1)
+    maxProdCuttingUtil(n, start, pos + 1),
+    (pos - start) * maxProdCuttingUtil(n, pos, pos + 1)
   );
 }
 
+
+
+int maxProdCuttingUtil2(int n){
+
+  if(n == 0) return 1;
+
+  int maxSol = INT_MIN;
+  for(int i = 1 ; i < n ; i++ ){
+
+    int tmpSol = max(
+      i * maxProdCuttingUtil2(n - i),
+      i * (n - i)
+    );
+
+    if(tmpSol > maxSol){
+      maxSol = tmpSol;
+    }
+
+  }
+
+  return maxSol;
+}
+
 int maxProdCutting(int n){
-  return wordBreakUtil(n, 0, 1);
+  return maxProdCuttingUtil2(n);
+  // return maxProdCuttingUtil(n, 0, 1);
 }
 
 
+int maxProdCuttingDP(int n){
 
+  int *dp = new int[n + 1];
+  memset(dp, 0, sizeof(int) * (n + 1));
+
+  dp[0] = 1;
+  dp[1] = 1;
+
+  for(int i = 2 ; i <= n ; i++){
+    for(int j = 1 ; j < i ; j++ ){
+
+      dp[i] = max(dp[i],
+        max(
+          j * dp[i - j],
+          j * (i - j)
+        )
+      );
+
+    }
+  }
+
+  return dp[n];
+}
 
 
 
@@ -34,19 +85,28 @@ int main(){
 
     n = 2, exp = 1;
     assert(maxProdCutting(n) == exp);
+    assert(maxProdCuttingDP(n) == exp);
 
     n = 3, exp = 2;
     assert(maxProdCutting(n) == exp);
+    assert(maxProdCuttingDP(n) == exp);
 
     n = 4, exp = 4;
     assert(maxProdCutting(n) == exp);
+    assert(maxProdCuttingDP(n) == exp);
 
     n = 5, exp = 6;
     assert(maxProdCutting(n) == exp);
+    assert(maxProdCuttingDP(n) == exp);
 
     n = 10, exp = 36;
     assert(maxProdCutting(n) == exp);
+    assert(maxProdCuttingDP(n) == exp);
 
+
+    for(int i = 1 ; i <= 10 ; i++){
+      cout << maxProdCutting(i) << endl;
+    }
 
     cout << "\n\nProgram is exiting..." << endl; 
     return 0; 
